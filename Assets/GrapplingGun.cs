bd5df44f36e945a655441c6ee0e25cd2ec3bdd3f
liamtheriot <liamtheriot@outlook.com>
2020-07-22 17:29:38 -0500

    grapple gun new scene rigidbody movment

---
+++ Assets/GrapplingGun.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform gunTip, camerapos, player;
+    public float maxDistance;
+    private SpringJoint joint;
+
+    [Space]
+    [Header("Spiring Joint Values")]
+    [SerializeField]private float desiredMaxDistanceMod = .8f;
+    [SerializeField] private float desiredMinDistanceMod = .25f;
+    [SerializeField] private float desiredSpringValue = 4.5f;
+    [SerializeField] private float desiredDampeningValue = 7f;
+    [SerializeField] private float desiredMassScale = 4.5f;
+
+
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        DrawRope();
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            StartGrapple();
+
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+    }
+
+    
+
+    private void StartGrapple()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(camerapos.position, camerapos.forward, out hit, maxDistance))
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+            joint.maxDistance = distanceFromPoint * desiredMaxDistanceMod;
+            joint.minDistance = distanceFromPoint * desiredMinDistanceMod;
+
+            joint.spring = desiredSpringValue;
+            joint.damper = desiredDampeningValue;
+            joint.massScale = desiredMassScale;
+        }
+
+    }
+    void DrawRope()
+    {
+        lr.SetPosition(0, gunTip.position);
+        lr.SetPosition(1, grapplePoint);
+
+    }
+
+    private void StopGrapple()
+    {
+
+    }
+}
---
+++ Assets/scripts/playermovement123.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playermovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    public float speeed;
+
+    Vector3 velocity;
+
+    public float gravity = -9.81f;
+
+    public Transform groundcheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpheight = 10f;
+    private float distToGround;
+    public float sprintSpeed = 0.5f;
+
+    public GameObject bulletPrefab;
+
+    public float health = 100f;
+        
+    CharacterController characterCollider;
+
+    public float crouchHeight = 2f;
+
+    public float regHeight = 4f;
+
+
+    private void Start()
+    {
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+
+        characterCollider = gameObject.GetComponent<CharacterController>();
+
+        speeed = speed;
+
+        
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            characterCollider.height = crouchHeight;
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            characterCollider.height = regHeight;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = speeed * sprintSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = speeed;
+        }
+
+
+
+        if (isGrounded() && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + .3f);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
